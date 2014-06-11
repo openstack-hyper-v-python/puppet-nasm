82296f57cdae014faf131d276ca006f0fbf41986
Tim Rogers <tiroger@microsoft.com>
2014-06-11 18:47:27 -0400

    various corrections and adjustments

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,33 @@
 class nasm (
     $version     = $nasm::params::version,
     $url         = $nasm::params::url,
-    $package     = $nasm::params:package,
+    $package     = $nasm::params::package,
     $nasmpath    = $nasm::params::nasmpath,
 ) inherits nasm::params {
-    windows_common::remote_file{"nasm":
+    windows_common::remote_file{'nasm':
         source      => "${url}",
         destination => "${nasmpath}\\${package}-${version}.zip",
         before      => Windows_7zip::Extract_file['nasm'],
-        require     => File["${$nasm}"],
+        require     => File["${nasmpath}"],
+    }
+    
+    file {"${nasmpath}":
+      ensure => directory,
     }
     
     windows_7zip::extract_file{'nasm':
         file        => "${nasmpath}\\${package}-${version}.zip",
         destination => $nasmpath,
-        before      => [exec['rename-nasm'], windows_path['nasm']],
+        before      => [File["${nasmpath}/nasmw.exe"], windows_path["$nasmpath"]],
         subscribe   => Windows_common::Remote_file["nasm"],
     }
     
-    exec{'rename-nasm':
-        command     => "rename ${nasmpath}\\nasm.exe nasmw.exe",
+    file{"${nasmpath}/nasmw.exe":
+      ensure => link,
+      target => "${nasmpath}/nasm.exe",
         require     => Windows_7zip::Extract_file['nasm'],
     }
-    
+
     windows_path{ $nasmpath:
         ensure      => present,
         require     => Windows_7zip::Extract_file['nasm'],
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
-class swig::params {
+class nasm::params {
     $version    = '2.07'
-    $url        = 'http://downloads.sourceforge.net/project/nasm/Win32%27binaries/2.07/nasm-2.07-win32.zip'
+    $url        = 'http://downloads.sourceforge.net/project/nasm/Win32%20binaries/2.07/nasm-2.07-win32.zip'
     $package    = 'nasm'
     $nasmpath   = 'C:\\nasm'
 }
